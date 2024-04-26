754b7fe3b43bc26db49307f87271eed81b9ae90f
Jakguy Boriboon <jakghuyb@gmail.com>
2024-04-26 17:07:00 +0700

    Revert "refactor and added video"
    This reverts commit 8b5d5dc96654dcc1b2f0de5bd69d949d7cf8e050.

--- guy-web/src/app/page.tsx
+++ guy-web/src/app/page.tsx
@@ -1,12 +1,8 @@
 import Image from "next/image";
-import Video from 'next-video';
-import harkonen from "../../videos/harkonen.mp4"
-
 
 export default function Home() {
   return (
     <main className="">
-      <Video src={harkonen} autoPlay loop controls={false} />;
     </main>
   );
 }
