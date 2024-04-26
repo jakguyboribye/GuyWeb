60992d208daa5c8fb7e47404a8cb3b105241d210
Jakguy Boriboon <jakghuyb@gmail.com>
2024-04-26 17:04:26 +0700

    refactor and added video

--- guy-web/src/app/page.tsx
+++ guy-web/src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import Video from 'next-video';
+import harkonen from "../../videos/harkonen.mp4"
+
 
 export default function Home() {
   return (
     <main className="">
+      <Video src={harkonen} autoPlay loop controls={false} />;
     </main>
   );
 }
